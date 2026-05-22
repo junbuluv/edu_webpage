2b4d769e4ab3e6651df1d95b60fdf26ec16ae85b
junbuluv <wnsgus1991@gmail.com>
2026-05-22 07:53:20 -0400

    Remove .vercel/ build artifacts from tracking

--- .vercel/output/_functions/chunks/content-modules_BIvT8ZMl.mjs
+++
@@ -1,7 +0,0 @@
-const contentModules = new Map([
-["src/content/lessons/macro/ad-as.mdx", () => import('./ad-as_4hRhVwc6.mjs')],
-["src/content/lessons/macro/is-lm-intro.mdx", () => import('./is-lm-intro_82qTA-Jj.mjs')],
-["src/content/lessons/macro/phillips-curve.mdx", () => import('./phillips-curve_C8gkwG2O.mjs')],
-["src/content/lessons/macro/solow.mdx", () => import('./solow_320rA3c8.mjs')]]);
-
-export { contentModules as default };
